cfeb236cc0bad82e0f96bbcf571cf1e6e502f013
foxytanuki <biz@foxytanuki.dev>
2024-05-12 23:35:41 +0900

    Succeed to send tx of offchain-logs

--- src/offchain-logs/main.go
+++ src/offchain-logs/main.go
@@ -1,22 +1,23 @@
 package main
 
 import (
+	"log"
+
 	"github.com/foxytanuki/suave-sandbox/framework"
 )
 
 func main() {
 	fr := framework.New()
-	fr.Suave.DeployContract("offchain-logs.sol/OffchainLogs.json")
-	// log.Default().Println(contract)
+	contract := fr.Suave.DeployContract("offchain-logs.sol/OffchainLogs.json")
 
-	// receipt := contract.SendConfidentialRequest("example", nil, nil)
-	// if len(receipt.Logs) != 2 {
-	// 	log.Fatal("two logs expected")
-	// }
+	receipt := contract.SendConfidentialRequest("example", nil, nil)
+	if len(receipt.Logs) != 2 {
+		log.Fatal("two logs expected")
+	}
 
-	// // emit the CCR but DO NOT leak the logs
-	// receipt = contract.SendConfidentialRequest("exampleNoLogs", nil, nil)
-	// if len(receipt.Logs) != 1 {
-	// 	log.Fatal("only one log expected")
-	// }
+	// emit the CCR but DO NOT leak the logs
+	receipt = contract.SendConfidentialRequest("exampleNoLogs", nil, nil)
+	if len(receipt.Logs) != 1 {
+		log.Fatal("only one log expected")
+	}
 }
