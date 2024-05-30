3407f867cddbc27aad125daf2e92c686978506ea
foxytanuki <biz@foxytanuki.dev>
2024-05-30 18:56:22 +0900

    Refactor is-confidential with suave.ts

--- src/ts/is-confidential/main.ts
+++ src/ts/is-confidential/main.ts
@@ -1,48 +1,26 @@
-import { http, encodeFunctionData } from "@flashbots/suave-viem";
+import { encodeFunctionData } from "@flashbots/suave-viem";
 import {
-  getSuaveWallet,
   SuaveTxRequestTypes,
   TransactionRequestSuave,
 } from "@flashbots/suave-viem/chains/utils";
-import { readFileSync } from "fs";
-import path from "path";
-
-const isTestnet = process.env.SUAVE_ENV === "rigil" || false;
-
-const SUAVE_RPC_URL = isTestnet
-  ? "https://rpc.rigil.suave.flashbots.net"
-  : "http://localhost:8545";
-// Change this to a private key with rETH you get from https://faucet.rigil.suave.flashbots.net/
-const PRIVATE_KEY = isTestnet
-  ? (`0x${process.env.PRIVATE_KEY}` as `0x${string}`) || undefined
-  : "0x91ab9a7e53c220e6210460b65a7a3bb2ca181412a8a7b43ff336b3df1737ce12";
+import Suave from "../suave.ts";
 
 // Change this to the address of the deployed contract
-const CONTRACT_ADDRESS = "0x9c2FD9E8BFA721e8691Bb727b53a891a5d96f923";
+const CONTRACT_ADDRESS = "0xd594760b2a36467ec7f0267382564772d7b0b73c";
 
-const wallet = getSuaveWallet({
-  transport: http(SUAVE_RPC_URL),
-  privateKey: PRIVATE_KEY,
-});
+const isTestnet = process.env.SUAVE_ENV === "rigil" || false;
+const suave = new Suave(isTestnet);
 
-const json = JSON.parse(
-  readFileSync(
-    path.resolve(
-      __dirname,
-      "../../../out/is-confidential.sol/IsConfidential.json"
-    )
-  ).toString()
+const kettleAddress = suave.getKettleAddress();
+const wallet = suave.getWallet();
+const { abi } = suave.getContractArtifacts(
+  "is-confidential.sol/IsConfidential.json"
 );
-const abi = json.abi;
 const data = encodeFunctionData({
   abi,
   functionName: "example",
   args: [],
 });
-
-const kettleAddress = isTestnet
-  ? "0x03493869959C866713C33669cA118E774A30A0E5"
-  : "0xB5fEAfbDD752ad52Afb7e1bD2E40432A485bBB7F";
 
 const ccr: TransactionRequestSuave = {
   confidentialInputs: "0x000",
