85c037a3b1347101cdc77d1b20c146bc630be3d5
foxytanuki <biz@foxytanuki.dev>
2024-06-03 02:58:45 +0900

    Refactor go/get-nonce

--- src/go/get-nonce/main.go
+++ src/go/get-nonce/main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/foxytanuki/suave-sandbox/framework"
 	"github.com/holiman/uint256"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	var log uint256.Int
-	fr := framework.New()
+	var logs uint256.Int
+	// Init godotenv
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Init framework
+	fr := framework.New(framework.WithCustomConfig(os.Getenv("PRIVATE_KEY"), os.Getenv("RPC_URL")))
 	contract := fr.Suave.DeployContract("getNonce.sol/GetNonce.json")
 	address := "0x39338FD37f41BabC04e119332198346C0EB31022"
 
@@ -18,8 +27,8 @@ func main() {
 	if len(receipt.Logs) != 1 {
 		panic("one log expected")
 	}
-	log.SetBytes(receipt.Logs[0].Data)
-	fmt.Println(log.String())
+	logs.SetBytes(receipt.Logs[0].Data)
+	log.Println(logs.String())
 
 	var log2 uint256.Int
 	receipt2 := contract.SendConfidentialRequest("example2", []interface{}{common.HexToAddress(address)}, nil)
@@ -27,5 +36,5 @@ func main() {
 		panic("one log expected")
 	}
 	log2.SetBytes(receipt2.Logs[0].Data)
-	fmt.Println(log2.String())
+	log.Println(log2.String())
 }
